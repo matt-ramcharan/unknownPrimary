566c64cb7433fb25b7c96a182b3daec1852465e6
ramcharan97 <matthew.a.ramcharan@gmail.com>
2019-07-23 21:11:37 +0100

    Data proprocessing finished and Matlab tested. Making own

---
+++ Python/DataFormatting/FileFormatting.py
@@ -0,0 +1,21 @@
+import pandas as pd
+from pathlib import Path
+
+
+dataDir = Path(r"C:\Users\matt-\Documents\Uni\TechnicalProject\GeneBinaryClass")
+breastPath = dataDir / "BreastData500.csv"
+colonPath = dataDir / "ColonData500.csv"
+
+breast = pd.read_csv(breastPath,index_col=0).T
+breast = breast.drop(['__no_feature', '__ambiguous', '__too_low_aQual',
+       '__not_aligned', '__alignment_not_unique'],axis=1)
+breast['Label'] = 'Breast'
+
+colon = pd.read_csv(colonPath,index_col=0).T
+colon = colon.drop(['__no_feature', '__ambiguous', '__too_low_aQual',
+       '__not_aligned', '__alignment_not_unique'],axis=1)
+colon['Label'] = 'Colon'
+
+
+dataset = pd.concat([breast, colon])
+dataset.to_csv('FullData.csv', index=False)
