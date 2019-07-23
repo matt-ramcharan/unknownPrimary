b64ea1552135766a3a6945b1fa7c0644a131760d
matt-ramcharan <matthew.a.ramcharan@gmail.com>
2019-07-23 23:41:37 +0100

    working with ROC curve. Needs checks for overfitting (validation)

--- Python/SupportVectorMachine/SVMMKLpyGeneData.py
+++ Python/SupportVectorMachine/SVMMKLpyGeneData.py
@@ -30,15 +30,18 @@ X = normalization(X) #||X_i||_2^2 = 1
 
 #train/test split
 from sklearn.model_selection import train_test_split
-Xtr,Xte,Ytr,Yte = train_test_split(X,Y, test_size=.25, random_state=42)
+Xtr,Xte,Ytr,Yte = train_test_split(X,Y, test_size=.25, random_state=42, shuffle=True)
 print ('done')
 
 
 #compute homogeneous polynomial kernels with degrees 0,1,2,...,10.
+degrees=2
 print ('computing Homogeneous Polynomial Kernels...', end='')
 from MKLpy.metrics import pairwise
-KLtr = [pairwise.homogeneous_polynomial_kernel(Xtr, degree=d) for d in range(11)]
-KLte = [pairwise.homogeneous_polynomial_kernel(Xte,Xtr, degree=d) for d in range(11)]
+# KLtr = [pairwise.homogeneous_polynomial_kernel(Xtr, degree=d) for d in range(degrees)]
+# KLte = [pairwise.homogeneous_polynomial_kernel(Xte,Xtr, degree=d) for d in range(degrees)]
+KLtr = [pairwise.homogeneous_polynomial_kernel(Xtr, degree=1)]
+KLte = [pairwise.homogeneous_polynomial_kernel(Xte,Xtr, degree=1)]
 print ('done')
 
 
@@ -73,3 +76,31 @@ print ('Accuracy score: %.3f, roc AUC score: %.3f' % (accuracy, roc_auc))
 # clf = EasyMKL(learner=base_learner)
 # clf = clf.fit(KLtr,Ytr)
 # print(clf)
+
+import matplotlib.pyplot as plt
+from sklearn.metrics import roc_curve, auc
+
+# Compute ROC curve and ROC area for each class
+fpr = dict()
+tpr = dict()
+roc_auc = dict()
+for i in range(2):
+    fpr[i], tpr[i], _ = roc_curve(Yte, y_score)
+    roc_auc[i] = auc(fpr[i], tpr[i])
+
+# Compute micro-average ROC curve and ROC area
+fpr["micro"], tpr["micro"], _ = roc_curve(Yte.ravel(), y_score.ravel())
+roc_auc["micro"] = auc(fpr["micro"], tpr["micro"])
+
+plt.figure()
+lw = 2
+plt.plot(fpr[1], tpr[1], color='darkorange',
+         lw=lw, label='ROC curve (area = %0.2f)' % roc_auc[1])
+plt.plot([0, 1], [0, 1], color='navy', lw=lw, linestyle='--')
+plt.xlim([0.0, 1.0])
+plt.ylim([0.0, 1.05])
+plt.xlabel('False Positive Rate')
+plt.ylabel('True Positive Rate')
+plt.title('Receiver operating characteristic example')
+plt.legend(loc="lower right")
+plt.show()
